d94dd3bbde73f0db290f8b5da71a049ecc39449b
Алакаев Булат <pakitos02@mail.ru>
2026-09-23 10:01:39 +0500

    Add ParameterizedTest and BeforeEach

--- src/test/java/SelenideLoginTest.java
+++ src/test/java/SelenideLoginTest.java
@@ -1,31 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
+import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Selenide.open;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class SelenideLoginTest {
 
     SelenideLoginPage loginPage = new SelenideLoginPage();
     SelenideProductsPage productsPage = new SelenideProductsPage();
 
+    @BeforeEach
+    void setUP(){
+        open("https://www.saucedemo.com/");
+    }
     @Test
     void successfulLoginTest() {
-         open("https://www.saucedemo.com/");
 
          loginPage.login("standard_user", "secret_sauce");
 
          productsPage.getPageTitle().shouldHave(text("Products"));
-        // $("[data-test='title']").shouldHave(text("Products"));
+
     }
-    @Test
-    void invalidPasswordTest() {
-        open("https://www.saucedemo.com/");
+    @ParameterizedTest
+    @CsvSource({
+            "standard_user, wrong_password",
+            "wrong_user, secret_sauce"
+    })
+    void invalidCredentialsTest(String username, String password){
 
-        loginPage.login("standard_user", "wrong_password");
-
+        loginPage.login(username,password);
         loginPage.getErrorMessage().shouldHave(text
                 ("Epic sadface: Username and password do not match any user in this service"));
-//        $("[data-test='error']").shouldHave(text
-//                ("Epic sadface: Username and password do not match any user in this service"));
-    }
+  }
 }
